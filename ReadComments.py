4777ab3604e6719049b0a709a91a091ef5f44ccd
Dinesh Prasanth M K <dmoluguw@redhat.com>
2019-10-02 16:46:03 -0400

    Add method to add comments
    Signed-off-by: Dinesh Prasanth M K <dmoluguw@redhat.com>

--- ReadComments.py
+++ ReadComments.py
@@ -6,12 +6,24 @@ base_pagure_url = "https://pagure.io"
 base_github_url = "https://api.github.com"
 
 
+def create_gh_head(gh_token):
+    header_params = {
+        "Content-Type": "application/json",
+        "Authorization": "token {0}".format(gh_token)
+    }
+    return header_params
+
+
+def convert_epoch_to_timestamp(epoch_time):
+    return time.strftime('%Y-%m-%d', time.localtime(epoch_time))
+
+
 def get_issues_from_pagure(project_name):
     params = {
         "status": "Open",
         "since": "2017-10-20",
         "order": "asc",
-        "per_page": "1"
+        "per_page": "5"
     }
     api_end_point = "/api/0/{0}/issues".format(project_name)
     results = []
@@ -39,42 +51,66 @@ def get_issues_from_pagure(project_name):
     return results
 
 
+def create_gh_comment(repo_space, project_name, issue_number, gh_token, comment):
+    api_end_point = "/repos/{0}/{1}/issues/{2}/comments".format(repo_space, project_name, issue_number)
+
+    create_issue_comment_url = base_github_url + api_end_point
+
+    print(create_issue_comment_url)
+
+    comment_content = "Posted by {0} on {1}: \n\n{2}".format(
+        comment['user']['name'],
+        convert_epoch_to_timestamp(int(comment['date_created'])),
+        comment['comment']
+    )
+
+    body_params = {
+        "body": comment_content.replace('@', '')
+    }
+
+    print(body_params)
+
+    response = requests.post(url=create_issue_comment_url, data=json.dumps(body_params), headers=create_gh_head(gh_token))
+
+    if response.status_code == 201:
+        print("Comment created successfully for issue: {0}".format(issue_number))
+    else:
+        print("Comment creation failed for issue {0} with status code {1}".format(issue_number, response.status_code))
+
+
 def create_gh_issue(repo_space, project_name, gh_token, record):
 
     api_end_point = "/repos/{0}/{1}/issues".format(repo_space, project_name)
 
     create_issue_url = base_github_url + api_end_point
 
-    print(create_issue_url)
-
-    header_params = {
-        "Content-Type": "application/json",
-        "Authorization": "token {0}".format(gh_token)
-    }
-
     issue_title = record["title"]
-    issue_desc = "Originally filed by {0} on {1}\n\n {2}".format(
+    issue_desc = "This issue was migrated from [pagure ticket #{0}](https://pagure.io/dogtagpki/issue/{0}). " \
+                 "Originally filed by {1} on {2}\n\n {3}".format(
+        record["id"],
         record["user"]["name"],
-        time.strftime('%Y-%m-%d', time.localtime(int(record["date_created"]))),
+        convert_epoch_to_timestamp(int(record["date_created"])),
         record["content"]
     )
 
     body_params = {
         "title": issue_title,
-        "body": issue_desc
+        "body": issue_desc.replace("@", "")
     }
-    if record["assignee"]:
-        body_params["assignee"] = record["assignee"]
+
+    # Uncomment if assignee is required
+    #if record["assignee"]:
+    #    body_params["assignee"] = record["assignee"]["name"]
 
     if len(record['tags']) > 0:
         for tag in record['tags']:
             body_params['labels'].append(tag)
 
+    print("BODY: " + json.dumps(body_params))
 
-    print(json.dumps(body_params))
+    response = requests.post(url=create_issue_url, data=json.dumps(body_params), headers=create_gh_head(gh_token))
 
-    print(header_params)
-    response = requests.post(url=create_issue_url, data=json.dumps(body_params), headers=header_params)
+    print (response.json())
 
     if response.status_code == 201:
         print('Created new GH issue for: {0}'.format(str(record['id'])))
@@ -85,38 +121,20 @@ def create_gh_issue(repo_space, project_name, gh_token, record):
         return
 
     # Add comments
-
-
-
-##
-#
-#     {
-#       "assignee": null,
-#       "blocks": [],
-#       "close_status": null,
-#       "closed_at": null,
-#       "closed_by": null,
-#       "comments": [],
-#       "content": "The `CertSearchRequest` interface does not admit a search like \"not revoked\" - it only allows one (positive) status match at a time.  Another example is all revoked certs (regardless whether expired), because there are two statuses involved: `REVOKED` and `REVOKED_EXPIRED`.\r\n\r\nUpdate the relevant types, routines, API and CLI interfaces to allow richer filtering by status.\r\n\r\nSee related FreeIPA bug: https://pagure.io/freeipa/issue/7549",
-#       "custom_fields": [],
-#       "date_created": "1569383294",
-#       "depends": [],
-#       "id": 3109,
-#       "last_updated": "1569383294",
-#       "milestone": null,
-#       "priority": null,
-#       "private": false,
-#       "status": "Open",
-#       "tags": [],
-#       "title": "Cert search: support multiple statuses",
-#       "user": {
-#         "fullname": "Fraser Tweedale",
-#         "name": "ftweedal"
-#       }
-#     }
+    for comment in record['comments']:
+        # Skip meta data comments
+        if not comment['comment'].startswith("**Metadata Update from"):
+            create_gh_comment(
+                repo_space=repo_space,
+                project_name=project_name,
+                issue_number=response.json()['number'],
+                gh_token=gh_token,
+                comment=comment
+            )
 
 
 if __name__ == "__main__":
+
     gh_token = input("Enter the GH Personal Access Token: ")
 
     issues = get_issues_from_pagure(project_name="dogtagpki")
