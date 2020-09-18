d0e81cf5b849cd99eec37777c14f95a204ed8724
Dinesh Prasanth M K <dmoluguw@redhat.com>
2020-09-17 19:01:57 -0700

    Add dogtagpki specific improvements
    This patch:
    
    - Refines to be used for dogtagpki specific usecase
    - Applies modified commit 2a51e38790c04c891f03deae3cfce065df5da430
    
    Signed-off-by: Dinesh Prasanth M K <dmoluguw@redhat.com>

--- README.md
+++ README.md
@@ -29,3 +29,6 @@ And then, you can just run the program.
     pagure2github update-pagure            # Stage 2. Update Pagure issues
     pagure2github update-bugzillas         # Stage 3. Update Bugzillas
     pagure2github close-unused-milestones  # Stage 4. Close Unused Milestones
+    pagure2github check-issue-statuses     # Stage 5. Check that all issues have the right statuses
+    pagure2github fix-references-comments  # Stage 6. Replace issue/PR links with \#0000 references
+    pagure2github fix-documentation-files  # Stage 7. Replace Pagure links with GitHub links in a tree
--- lib/pagure2github/pagure.py
+++ lib/pagure2github/pagure.py
@@ -11,24 +11,8 @@
 # See LICENSE for details.
 # --- END COPYRIGHT BLOCK ---
 
-import json
 import textwrap
-from os import listdir
-from os.path import isfile, join
 from libpagure import Pagure
-
-ISSUES_DIR = "tickets"
-PRS_DIR = "requests"
-
-
-def fetch_issues():
-    issues = [f for f in listdir(ISSUES_DIR) if isfile(join(ISSUES_DIR, f))]
-    issue_jsons = []
-    for issue in issues:
-        with open(join(ISSUES_DIR, issue)) as f:
-            issue_jsons.append(json.loads(f.read()))
-    return issue_jsons
-
 
 class PagureWorker:
     def __init__(self, repo, api_key, log):
@@ -38,41 +22,26 @@ class PagureWorker:
     def comment_on_issue(self, pg_issue_id, gh_issue_id):
         msg = textwrap.dedent(
             f"""
-        389-ds-base is moving from Pagure to Github. This means that new issues and pull requests
-        will be accepted only in [389-ds-base's github repository](https://github.com/389ds/389-ds-base).
+        dogtagpki is moving from Pagure issues to Github issues. This means that new issues
+        will be reported only in [dogtagpki's github repository](https://github.com/dogtagpki/pki).
 
         This issue has been cloned to Github and is available here:
-        - https://github.com/389ds/389-ds-base/issues/{gh_issue_id}
+        - https://github.com/dogtagpki/pki/issues/{gh_issue_id}
 
-        If you want to receive further updates on the issue, please navigate [to the github issue](https://github.com/389ds/389-ds-base/issues/{gh_issue_id})
+        If you want to receive further updates on the issue, please navigate [to the github issue](https://github.com/dogtagpki/pki/issues/{gh_issue_id})
         and click on `subscribe` button.
 
         Thank you for understanding. We apologize for all inconvenience.
         """
         ).strip()
+        self.log.info(f"Updating issue {pg_issue_id} with {gh_issue_id}")
         self.api.comment_issue(pg_issue_id, msg)
 
     def close_issue(self, pq_issue_id, status=None):
         if status is None:
             status = "Fixed"
+        self.log.info(f"Closing issue {pq_issue_id} with '{status}' status")
         self.api.change_issue_status(pq_issue_id, "Closed", status)
-
-    def comment_on_pull_request(self, pg_pr_id, gh_issue_id):
-        msg = textwrap.dedent(
-            f"""
-        389-ds-base is moving from Pagure to Github. This means that new issues and pull requests
-        will be accepted only in [389-ds-base's github repository](https://github.com/389ds/389-ds-base).
-
-        This pull request has been cloned to Github as issue and is available here:
-        - https://github.com/389ds/389-ds-base/issues/{gh_issue_id}
-
-        If you want to continue to work on the PR, please navigate [to the github issue](https://github.com/389ds/389-ds-base/issues/{gh_issue_id}),
-        download the patch from the attachments and file a new pull request.
-
-        Thank you for understanding. We apologize for all inconvenience.
-        """
-        ).strip()
-        self.api.comment_request(pg_pr_id, msg)
 
     def close_pull_request(self, pg_pr_id):
         self.api.close_request(pg_pr_id)
